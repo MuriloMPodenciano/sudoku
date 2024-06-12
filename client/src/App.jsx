f7d1a43104e5012a3f5b2be416eca74fd6794ac2
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-12 05:43:22 -0300

    feat: added board route to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
+import Board from './components/Board.jsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Signup />} />
       <Route path="/register" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/board" element={<Board />} />
     </Routes>
   </Router>
   );
