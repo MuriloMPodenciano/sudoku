2c8055005fc2ba0b43cb4584fd845cfe89ae1fbf
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-12 14:02:47 -0300

    feat: made it so the user can only access the board once with a valid JWT token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Board from './components/Board.jsx';
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+      return <Navigate to="/login" />;
+  }
+
+  return children;
+};
 
 const App = () => {
   return (
@@ -12,7 +22,14 @@ const App = () => {
       <Route path="/" element={<Signup />} />
       <Route path="/register" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/board" element={<Board />} />
+      <Route
+        path="/board"
+        element={
+          <PrivateRoute>
+            <Board />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </Router>
   );
