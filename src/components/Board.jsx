0d4b6248034590ed44c47175ee26a85613b6c40d
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-12 01:47:44 -0300

    feat: made function to handle clicks in tiles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,19 +14,25 @@ const Board = () => {
         "81--45---"
     ]);
 
+    const [selectedTile, setSelectedTile] = useState(null);
+
+
     useEffect(() => {
         const gameBoard = document.getElementById('board');
         gameBoard.innerHTML = ''; 
 
         for (let row = 0; row < 9; row++) {
-          for (let column = 0; column < 9; column++) {
-            const tile = document.createElement("div");
-            tile.id = row.toString() + "-" + column.toString();
+            for (let column = 0; column < 9; column++) {
+                const tile = document.createElement("div");
+                tile.id = row.toString() + "-" + column.toString();
 
             if (board[row][column] != "-" ){
                 tile.textContent = board[row][column];
                 tile.classList.add("tile-start"); 
+            }else {
+                tile.addEventListener("keydown", handleKeyDown);
             }
+            tile.addEventListener('click', () => handleTileClick(row, column));
 
             if (row === 2 || row === 5) {
                 tile.classList.add("horizontal-line");
@@ -35,12 +41,25 @@ const Board = () => {
                 tile.classList.add("vertical-line");
             }
 
+            if (selectedTile[0] === row && selectedTile[1] === column) {
+                tile.classList.add("selected");
+            }
+
             tile.classList.add("tile");
             gameBoard.appendChild(tile);
-          }
+            }
         }
-      }, []);
+    }, [board]);
 
+    const handleTileClick = (row, column) => {
+        const tile = document.getElementById(row.toString() + "-" + column.toString());
+        if (!tile.classList.contains("tile-start")) {
+            setSelectedTile([row, column]);
+            tile.focus();
+        } 
+    };
+
+    
     return (
         <div>
             <h1>Sudoku</h1>
