e31315bb909845a73374ee8bee1ca7994c89e2b7
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-06-12 00:57:34 -0300

    feat: created basic implementation for board and linked it to app.js

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Board = () => {
+
+    return (
+        <div>
+            <h1>Sudoku</h1>
+            <hr></hr>
+            <div id='board'></div>
+        </div>
+    );
+};
+
+export default Board
